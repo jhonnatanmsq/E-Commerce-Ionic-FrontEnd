161084c6a0d2ea8ec4ce5dcfcf9a5ce5bf60d324
Jhonnatan Mesquita <jhonnatanmesquita@gmail.com>
2019-09-07 12:34:20 -0300

    Tratando erros 403

--- src/app/_interceptors/error-interceptor.ts
+++ src/app/_interceptors/error-interceptor.ts
@@ -2,10 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { StorageService } from '../_services/storage.service';
+import { throwError } from 'rxjs';
 
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
+
+    constructor (private storage : StorageService){}
 
     intercept(req : HttpRequest<any>, next : HttpHandler) : Observable<HttpEvent<any>>{
         return next.handle(req)
@@ -22,9 +26,19 @@ export class ErrorInterceptor implements HttpInterceptor{
 
             console.log(errorObj);
 
-            return Observable.throw(errorObj);
+            switch(errorObj.status){
+                case 403:
+                    this.handle403();
+                    break;
+            }
+
+            return throwError(errorObj);
          })
         ) as any;
+    }
+
+    handle403(){
+        this.storage.setLocalUser(null);
     }
 }
 
--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { StorageService } from '../_services/storage.service';
 import { ClienteDTO } from '../_models/cliente.dto';
 import { ClienteService } from '../_services/domain/cliente.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -12,7 +13,7 @@ export class ProfilePage implements OnInit {
 
   cliente : ClienteDTO;
 
-  constructor( private storage : StorageService, private clienteService : ClienteService) { }
+  constructor(private router: Router, private storage : StorageService, private clienteService : ClienteService) { }
 
   ngOnInit() {
     let localUser = this.storage.getLocalUser();
@@ -20,7 +21,13 @@ export class ProfilePage implements OnInit {
       this.clienteService.findByEmail(localUser.email)
         .subscribe(res => {
           this.cliente = res;
-        }, error => {});
+        }, error => {
+          if(error.status == 403){
+            this.router.navigate(['/home']);
+          }
+        });
+    }else{
+      this.router.navigate(['/home']);
     }
   }
 
